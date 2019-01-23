c529e0a892c7d641eb2d622a8dc0141b03dccf2e
VARUN REDDY <44882779+varun310794@users.noreply.github.com>
2019-01-23 09:37:08 -0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,25 @@
+var username=document.getElementById("1").value;
+var email=document.getElementById("2").value;
+var password=document.getElementById("3").value;
+var confirmpassword=document.getElementById("4").value;
+// var signin=document.getElementById("btnsignup");
+// alert(878)
+// signin.addEventListener("click", function(){
+
+//     alert(username);
+//     document.write(username);
+//     document.write(email);
+//     document.write(password);
+//     document.writeln(confirmpassword);
+//     console.log("what the hell ??");
+// });
+
+function hello(){
+
+    document.write("username");
+    document.write("email");
+    document.write(password);
+    document.writeln(confirmpassword);
+    console.log(username);
+
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+var port = 85,
+
+express = require('express'),
+
+app = express().use(express.static(__dirname + '/')),
+
+http = require('http').Server(app);
+
+ 
+
+app.use('/', function(req, res){
+
+    res.sendFile(__dirname + '/login.html');
+});
+
+ 
+
+http.listen(port, function(){
+
+    console.log("Node server listening on port " + port);
+
+});
+
+ 
